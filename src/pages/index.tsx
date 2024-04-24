0dfe9b44c76ac14f37577bb8e36f71d34142d3db
Kirigami <89954963+kirigamilabs@users.noreply.github.com>
2024-04-24 15:08:54 -0500

    Update update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,46 +83,10 @@ export default function Home({}: HomeProps) {
         </ShowcaseSection>
 
         {/* The Future is Now */}
-        <Section py={sectionPaddingY} gap={12}>
-          <ShowcaseSection startWithVisual px={0}>
-            <ShowcaseContent title="The Future is Now" id="more">
-              <Text>
-                Kirigami is testing the boundaries of what&apos;s possible in today&apos;s world.
-                Check out these ways that we can help you and your business take a leap forward into the world of tomorrow.
-              </Text>
-            </ShowcaseContent>
-            <ShowcaseVisual>
-              <Triangles variant="triforce" />
-            </ShowcaseVisual>
-          </ShowcaseSection>
-
-          <ContributingCards />
-
-          <Flex justify="center">
-            <ButtonLink href={'/use-cases'} variant="solid">
-              Check out our Use Cases
-            </ButtonLink>
-          </Flex>
-        </Section>
 
 
         {/* Playground section */}
-        <Section py={sectionPaddingY} gap={12}>
-          <ShowcaseSection startWithVisual>
-            <ShowcaseContent title="The AI x Crypto Revolution">
-              <Text>
-                Try some of our products for yourself in this simple playground. To learn more, feel free to contact us for a demo or follow along as we divulge our learnings on our {' '}
-                <Link href="https://kirigami.substack.com">Substack</Link>.
-              </Text>
-            </ShowcaseContent>
-            <ShowcaseVisual>
-              <Triangles variant="double" />
-            </ShowcaseVisual>
-          </ShowcaseSection>
 
-          {/* Interactive Solidity code editor and compiler */}
-          <CompilerPlayground />
-        </Section>
       </Box>
     </>
   )
